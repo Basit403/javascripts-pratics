6a519a7440e0cd9759fc1963cdaca75e57811071
Bashiru basit <bashirubasit8@gmail.com>
2022-12-08 15:00:06 +0100

    created a new folder

---
+++ javascripts pratice/test.js
@@ -0,0 +1,41 @@
+function multiply (a, b){
+
+    var times = (a * b);
+    return times;
+
+}
+
+var Myoutcome = multiply (3,4);
+document.write(Myoutcome);
+
+function division (a, b){
+
+    var divide = (a / b);
+    return divide;
+
+}
+
+var finalanswer = division (8,2);
+document.write(finalanswer);
+
+function subtraction (a, b){
+
+    var minus = (a - b);
+    return minus;
+
+}
+
+var myresult = subtraction (9,2);
+document.write(myresult);
+
+function addition (a, b){
+
+    var sum = (a + b);
+    return sum;
+
+}
+
+var answer = addition (4,9);
+document.write(answer);
+
+
