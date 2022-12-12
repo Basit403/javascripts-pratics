aa87a63a78f3937f1c3e8e795c68a9279aba7169
Bashiru basit <bashirubasit8@gmail.com>
2022-12-12 15:18:48 +0100

    updated my code

--- javascripts pratice/test.js
+++ javascripts pratice/test.js
@@ -1,55 +1,7 @@
-function multiply (a, b){
-
-    var times = (a * b);
-    return times;
-
-}
-
-var Myoutcome = multiply (3,4);
-document.write(Myoutcome);
-
-function division (a, b){
-
-    var divide = (a / b);
-    return divide;
+function add(a,b,c,boy){
+    
+    return a + b + c;
 
 }
 
-var finalanswer = division (8,2);
-document.write(finalanswer);
-
-function subtraction (a, b){
-
-    var minus = (a - b);
-    return minus;
-
-}
-
-var myresult = subtraction (9,2);
-document.write(myresult);
-
-function addition (a, b){
-
-    var sum = (a + b);
-    return sum;
-
-}
-
-var answer = addition (4,9);
-document.write(answer);
-
-
-
-function addition(){
-
-    var sum = 0;
-    for ( e = 0; e < arguments.length; e++) {
-        sum += arguments[e];
-        
-    }
-
-    console.log(sum)
-    return sum;
-
-    addition(2,4,6,8,10,11)
-}
+document.write(add(3,4,5))
