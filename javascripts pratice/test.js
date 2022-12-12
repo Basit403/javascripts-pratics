41d949c353553640e53b414a2d4362a03302e374
Bashiru basit <bashirubasit8@gmail.com>
2022-12-12 16:28:45 +0100

    updated my code

--- javascripts pratice/test.js
+++ javascripts pratice/test.js
@@ -1,7 +1,12 @@
-function add(a,b,c,boy){
-    
-    return a + b + c;
+function sum() {
 
+    let total = 0;
+    for (let i of arguments) {
+        total += i;
+        
+    }  
+    
+    return total;
 }
 
-document.write(add(3,4,5))
+document.write(sum(2,3,4,5,9,8,3))
